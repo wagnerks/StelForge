a3aaeb43280bd7e56c91309104b9bf49171a37aa
Kirill Vagner <kirill.vagner@mytona.com>
2022-07-24 18:20:19 +0300

    few utils functions

---
+++ src/renderModule/Utils.cpp
@@ -0,0 +1,48 @@
+﻿#include "Utils.h"
+
+#include "mat4x4.hpp"
+#include "Renderer.h"
+
+using namespace GameEngine::RenderModule;
+
+void Utils::CalculateEulerAnglesFromView(const glm::mat4& view, float& yaw, float& pitch, float& roll) {
+	if (view[0][0] == 1.0f || view[0][0] == -1.0f) {
+		yaw = atan2f(view[0][2], view[2][3]);
+		pitch = 0;
+		roll = 0;
+	}
+	else {
+		yaw = atan2(-view[2][0], view[0][0]);
+		pitch = asin(view[1][0]);
+		roll = atan2(-view[1][2], view[1][1]);
+	}
+}
+
+void Utils::renderQuad() {
+	static unsigned quadVAO = 0;
+	if (quadVAO == 0) {
+		float quadVertices[] = {
+			// positions        // texture Coords
+			0.70f, 1.0f, 0.0f, 0.0f, 1.0f,
+			0.70f, 0.70f, 0.0f, 0.0f, 0.0f,
+			1.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+			1.0f, 0.7f, 0.0f, 1.0f, 0.0f,
+		};
+		// setup plane VAO
+		unsigned quadVBO;
+		glGenVertexArrays(1, &quadVAO);
+		glGenBuffers(1, &quadVBO);
+		glBindVertexArray(quadVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	}
+	glBindVertexArray(quadVAO);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	RenderModule::Renderer::drawCallsCount++;
+	RenderModule::Renderer::drawVerticesCount += 4;
+	glBindVertexArray(0);
+}
---
+++ src/renderModule/Utils.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <fwd.hpp>
+
+namespace GameEngine::RenderModule {
+	class Utils {
+	public:
+		//angles in radians
+		static void CalculateEulerAnglesFromView(const glm::mat4& view, float& yaw, float& pitch, float& roll);
+		static void renderQuad();
+	};
+}
