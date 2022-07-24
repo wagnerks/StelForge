63ab866538ede019fbe0e2a0da67b4e558954d1a
Kirill Vagner <kirill.vagner@mytona.com>
2022-07-24 19:19:39 +0300

    added scene class draft

---
+++ src/core/Scene.cpp
@@ -0,0 +1,15 @@
+﻿#include "Scene.h"
+
+using namespace GameModule::CoreModule;
+
+void Scene::init() {
+	rootNode = new GameEngine::NodeModule::Node("root");
+	skybox = new GameEngine::RenderModule::Skybox("skybox/");
+	skybox->init();
+}
+void Scene::updateScene(float dt) {
+	
+}
+void Scene::drawScene() {
+	skybox->draw();
+}
---
+++ src/core/Scene.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "nodeModule/Node.h"
+#include "renderModule/Skybox.h"
+
+namespace GameModule::CoreModule {
+	class Scene {
+	public:
+		void init();
+		void updateScene(float dt);
+		void drawScene();
+	private:
+		GameEngine::NodeModule::Node* rootNode = nullptr;
+		GameEngine::RenderModule::Skybox* skybox = nullptr;
+	};
+}
+
