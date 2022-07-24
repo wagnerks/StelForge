9dfd789d999d757e5c06adaeebb798dc6ca7d843
Kirill Vagner <kirill.vagner@mytona.com>
2022-07-24 18:25:08 +0300

    texture according to new textureHandler class update

--- src/core/texture.cpp
+++ src/core/texture.cpp
@@ -3,46 +3,22 @@
 #include "stb_image.h"
 #include "glad/glad.h"
 #include "logsModule/logger.h"
+#include "renderModule/TextureHandler.h"
 
 using namespace GameEngine;
 using namespace GameEngine::Render;
-
-unsigned Texture::loadTexture(const std::string& path) {
-	stbi_set_flip_vertically_on_load(false);
-	unsigned texID;
-
-	int texWidth, texHeight, nrChannels;
-
-	auto data = stbi_load(path.c_str(), &texWidth, &texHeight, &nrChannels, 4);
-
-	glGenTextures(1, &texID);
-
-	glBindTexture(GL_TEXTURE_2D, texID);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, texWidth, texHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-
-	return texID;
-}
-
-void Texture::init(const std::string& path) {
-	data = load(path);
-}
 
 unsigned Texture::getTextureID() const {
 	return texID;
 }
 
 void Texture::bind(unsigned int slot) {
-	glActiveTexture(GL_TEXTURE0 + slot);
-	glBindTexture(GL_TEXTURE_2D, texID);
+	this->slot = slot;
+	RenderModule::TextureHandler::getInstance()->bindTexture(slot, GL_TEXTURE_2D, texID);
 }
 
 void Texture::unbind() {
-	glBindTexture(GL_TEXTURE_2D, 0);
+	RenderModule::TextureHandler::getInstance()->bindTexture(slot, GL_TEXTURE_2D, 0);
 }
 
 Texture::~Texture() {
@@ -51,36 +27,11 @@ Texture::~Texture() {
 
 void Texture::clear() {
 	if (texID) {
-		stbi_image_free(data);
 		glDeleteTextures(1, &texID);
 	}
 }
 
-unsigned char* Texture::load(const std::string& path) {
+unsigned int Texture::load(const std::string& path) {
 	clear();
-
-	stbi_set_flip_vertically_on_load(true);
-
-	texturePath = path;
-
-	data = stbi_load(texturePath.c_str(), &texWidth, &texHeight, &nrChannels, 4);
-
-	glGenTextures(1, &texID);
-
-	glBindTexture(GL_TEXTURE_2D, texID);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, texWidth, texHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-	unbind();
-
-	//glGenerateMipmap(GL_TEXTURE_2D);
-	
-	if (!data) {
-		LogsModule::Logger::LOG_WARNING("Can't load texture %s", path.c_str());
-	}
-	
-	return data;
+	return RenderModule::TextureHandler::getInstance()->loader.loadTexture(path, true);
 }
--- src/core/texture.h
+++ src/core/texture.h
@@ -4,8 +4,6 @@
 namespace GameEngine::Render {
 	class Texture {
 	public:
-		static unsigned int loadTexture(const std::string& path);
-		void init(const std::string& path);
 		unsigned int getTextureID() const;
 		void bind(unsigned int slot);
 		void unbind();
@@ -13,13 +11,9 @@ namespace GameEngine::Render {
 		~Texture();
 	private:
 		void clear();
-		unsigned char* load(const std::string& path);
-		unsigned char* data = nullptr;
+		unsigned int load(const std::string& path);
 		unsigned int texID = 0;
-		std::string texturePath;
-		int texWidth = 0;
-		int texHeight = 0;
-		int nrChannels = 0;
+		unsigned slot = 0;
 	};
 }
 
