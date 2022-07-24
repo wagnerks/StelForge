e510d76a231e0492ed5b507b0aca1fa76904bf5b
Kirill Vagner <kirill.vagner@mytona.com>
2022-07-24 18:27:02 +0300

    added some info about shaders in shaders debug

--- src/debugModule/ShadersDebug.cpp
+++ src/debugModule/ShadersDebug.cpp
@@ -5,6 +5,7 @@
 #include "imgui.h"
 
 #include "shaderModule/ShaderController.h"
+#include "shaderModule/GeometryShader.h"
 
 using namespace GameEngine::Debug;
 
@@ -14,13 +15,24 @@ void ShadersDebug::shadersDebugDraw(bool& opened) {
 	}
 
 	ImGui::Begin("shaders", &opened);
-	auto paths = SHADER_CONTROLLER->getShaderPaths();
 	for (auto& [hash, shader] : SHADER_CONTROLLER->getShaders()) {
 
 		auto shaderIdString = std::to_string(shader->getID());
 		if (ImGui::TreeNode(shaderIdString.c_str())) {
-			ImGui::Text("vertexPath: %s", paths[hash].first.c_str());
-			ImGui::Text("vertexPath: %s", paths[hash].second.c_str());
+			if (auto vfShader = dynamic_cast<ShaderModule::Shader*>(shader)) {
+				ImGui::Text("VERTEX FRAGMENT SHADER");
+
+				ImGui::Text("vertexPath: %s", vfShader->getVertexPath().data());
+				ImGui::Text("fragmentPath: %s", vfShader->getFragmentPath().data());
+			}
+			else if (auto gShader = dynamic_cast<ShaderModule::GeometryShader*>(shader)) {
+				ImGui::Text("GEOMETRY SHADER");
+
+				ImGui::Text("vertexPath: %s", gShader->getVertexPath().data());
+				ImGui::Text("fragmentPath: %s", gShader->getFragmentPath().data());
+				ImGui::Text("geometryPath: %s", gShader->getGeometryPath().data());
+			}
+			
 			auto btnId = "reCompile##" + std::to_string(shader->getID());
 			if(ImGui::Button(btnId.c_str())) {
 				SHADER_CONTROLLER->recompileShader(shader);
