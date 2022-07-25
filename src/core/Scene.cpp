35effe2e03980927d8c1037ef48055e8b1a77b35
Kirill Vagner <kirill.vagner@mytona.com>
2022-07-25 22:44:53 +0300

    add light to scene

--- src/core/Scene.cpp
+++ src/core/Scene.cpp
@@ -6,6 +6,8 @@
 #include "debugModule/ComponentsDebug.h"
 #include "modelModule/MeshFactory.h"
 #include "shaderModule/ShaderController.h"
+#include "renderModule/TextureHandler.h"
+#include "renderModule/Utils.h"
 
 using namespace GameModule::CoreModule;
 
@@ -21,6 +23,15 @@ void Scene::init() {
 	auto box = new GameEngine::NodeModule::Node("box1");
 	box->getComponent<MeshComponent>()->setMesh(GameEngine::ModelModule::MeshFactory::createPrimitiveMesh(GameEngine::ModelModule::eDrawObjectType::CUBE));
 	rootNode->addElement(box);
+
+	auto box2 = new GameEngine::NodeModule::Node("box2");
+	box2->getComponent<MeshComponent>()->setMesh(GameEngine::ModelModule::MeshFactory::createPrimitiveMesh(GameEngine::ModelModule::eDrawObjectType::CUBE));
+	box2->getComponent<TransformComponent>()->setScale({5.f,1.f,5.f});
+	box2->getComponent<TransformComponent>()->setY(-1.f);
+
+	rootNode->addElement(box2);
+
+	sunLight = new GameEngine::LightsModule::DirectionalLight();
 }
 
 void Scene::updateScene(float dt) {
@@ -31,19 +42,73 @@ void Scene::updateScene(float dt) {
 		GameEngine::Debug::ComponentsDebug::transformComponentDebug(node->getId(), nodeTC);
 		nodeTC->reloadTransform();
 	}
+
+	GameEngine::Debug::ComponentsDebug::transformComponentDebug("light", sunLight->getComponent<TransformComponent>());
+	sunLight->getComponent<TransformComponent>()->reloadTransform();
 }
 
 void Scene::drawScene() {
-	auto simpleLight = SHADER_CONTROLLER->loadVertexFragmentShader("shaders/light.vs", "shaders/light.fs");
-	simpleLight->use();
-	simpleLight->setMat4("projection", GameEngine::Engine::getInstance()->getCamera()->getProjectionsMatrix());
-	simpleLight->setMat4("view", GameEngine::Engine::getInstance()->getCamera()->GetViewMatrix());
+	auto simpleDepthShader = SHADER_CONTROLLER->loadVertexFragmentShader("shaders/depth.vs", "shaders/depth.fs");
+	auto shader = SHADER_CONTROLLER->loadVertexFragmentShader("shaders/objectWithShadow.vs", "shaders/objectWithShadow.fs");
+    shader->use();
+    shader->setInt("diffuseTexture", 0);
+    shader->setInt("shadowMap", 1);
+
+	unsigned int woodTexture = GameEngine::RenderModule::TextureHandler::getInstance()->loader.loadTexture("test.png");
 	
+	sunLight->preDraw();
+
+
 	for (auto node : rootNode->getAllNodes()) {
 		auto nodeTC = node->getComponent<TransformComponent>();
-		simpleLight->setMat4("model", nodeTC->getTransform());
-		node->getComponent<MeshComponent>()->getMesh()->draw(simpleLight);
+		simpleDepthShader->setMat4("model", nodeTC->getTransform());
+		node->getComponent<MeshComponent>()->getMesh()->draw(simpleDepthShader);
 	}
+
+	sunLight->postDraw();
+
+
+    shader->use();
+
+    glm::mat4 projection = GameEngine::Engine::getInstance()->getCamera()->getProjectionsMatrix();
+    glm::mat4 view = GameEngine::Engine::getInstance()->getCamera()->GetViewMatrix();
+
+    shader->setMat4("projection", projection);
+    shader->setMat4("view", view);
+    // set light uniforms
+    shader->setVec3("viewPos", GameEngine::Engine::getInstance()->getCamera()->Position);
+    shader->setVec3("lightPos", sunLight->getComponent<TransformComponent>()->getPos());
+    shader->setMat4("lightSpaceMatrix", sunLight->getLightSpaceProjection());
+	GameEngine::RenderModule::TextureHandler::getInstance()->bindTexture(GL_TEXTURE0, GL_TEXTURE_2D, woodTexture);
+	GameEngine::RenderModule::TextureHandler::getInstance()->bindTexture(GL_TEXTURE1, GL_TEXTURE_2D, sunLight->getDepthMapTexture());
+
+	for (auto node : rootNode->getAllNodes()) {
+		auto nodeTC = node->getComponent<TransformComponent>();
+		shader->setMat4("model", nodeTC->getTransform());
+		node->getComponent<MeshComponent>()->getMesh()->draw(shader);
+	}
+
+	auto scale = sunLight->getComponent<TransformComponent>()->getScale();
+	sunLight->getComponent<TransformComponent>()->setScale({0.5f,0.5f,0.5f});
+	sunLight->getComponent<TransformComponent>()->reloadTransform();
+	shader->setMat4("model", sunLight->getComponent<TransformComponent>()->getTransform());
+	GameEngine::RenderModule::Utils::renderCube();
+	sunLight->getComponent<TransformComponent>()->setScale(scale);
+	sunLight->getComponent<TransformComponent>()->reloadTransform();
+
+	auto lines = SHADER_CONTROLLER->loadVertexFragmentShader("shaders/xyzLines.vs", "shaders/xyzLines.fs");
+	lines->use();
+	lines->setMat4("PVM",GameEngine::Engine::getInstance()->getCamera()->getProjectionsMatrix() * GameEngine::Engine::getInstance()->getCamera()->GetViewMatrix() *  sunLight->getComponent<TransformComponent>()->getTransform());
+	GameEngine::RenderModule::Utils::renderXYZ(20.f);
+
+	
+	GameEngine::RenderModule::TextureHandler::getInstance()->bindTexture(GL_TEXTURE1, GL_TEXTURE_2D, sunLight->getDepthMapTexture());
+	auto drawImageShader = SHADER_CONTROLLER->loadVertexFragmentShader("shaders/debugDepth.vs", "shaders/debugDepth.fs");
+	drawImageShader->use();
+	drawImageShader->setInt("depthMap", 1);
+	drawImageShader->setFloat("near_plane", sunLight->getNearPlane());
+	drawImageShader->setFloat("far_plane", sunLight->getFarPlane());
+	GameEngine::RenderModule::Utils::renderQuad(0.7f, -0.2f,1.0f,0.1f);
 
 	skybox->draw();
 	gridFloor->draw();
@@ -53,4 +118,5 @@ Scene::~Scene() {
 	delete rootNode;
 	delete skybox;
 	delete gridFloor;
+	delete sunLight;
 }
--- src/core/Scene.h
+++ src/core/Scene.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include "nodeModule/Node.h"
+#include "renderModule/DirectionalLight.h"
 #include "renderModule/SceneGridFloor.h"
 #include "renderModule/Skybox.h"
 
@@ -14,6 +15,7 @@ namespace GameModule::CoreModule {
 		GameEngine::NodeModule::Node* rootNode = nullptr;
 		GameEngine::RenderModule::Skybox* skybox = nullptr;
 		GameEngine::RenderModule::SceneGridFloor* gridFloor = nullptr;
+		GameEngine::LightsModule::DirectionalLight* sunLight = nullptr;
 	};
 }
 
