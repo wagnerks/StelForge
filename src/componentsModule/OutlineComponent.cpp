3a7d73c92edcc5057767abaf75e2e1c23677f82e
Kirill Vagner <stalker1177@mail.ru>
2023-09-20 20:21:03 +0200

    added outline component

---
+++ src/componentsModule/OutlineComponent.cpp
@@ -0,0 +1,1 @@
+﻿#include "OutlineComponent.h"
---
+++ src/componentsModule/OutlineComponent.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include "ecsModule/ComponentBase.h"
+
+namespace Engine::ComponentsModule {
+	class OutlineComponent : public ecsModule::Component<OutlineComponent> {}; //
+}
+
+using Engine::ComponentsModule::OutlineComponent;
