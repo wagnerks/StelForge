a0ea09a8ed00d855a7124744d66a6fe12d63b37b
Kirill Vagner <stalker1177@mail.ru>
2023-09-20 20:21:31 +0200

    added shaders system priority

--- src/systemsModule/SystemsPriority.h
+++ src/systemsModule/SystemsPriority.h
@@ -4,13 +4,14 @@ enum eSystemsPriority : uint16_t {
 	DEFAULT = 0,
 	TRANSFORM_SYSTEM,
 	LOD_SYSTEM,
+	SHADERS_SYSTEM,
 	RENDER_SYSTEM
 };
 
 enum eRenderPassPriority : size_t {
 	CASCADE_SHADOWS = 0,
 	GEOMETRY,
+	SHADERS,
 	SSAO,
 	LIGHTING,
-
 };
