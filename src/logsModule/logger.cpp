549f0694a0ebbe43ed4dc34799465e5585c93225
Kirill Vagner <stalker1177@mail.ru>
2023-10-16 18:58:56 +0200

    added time to log

--- src/logsModule/logger.cpp
+++ src/logsModule/logger.cpp
@@ -2,24 +2,33 @@
 
 #include <cassert>
 #include <REND.h>
+#include "chrono"
 
 using namespace Engine::LogsModule;
 
 void Logger::logMessage(const eLogLevel level, const char* msg) {
+	auto t = time(nullptr);
+	
+	std::ostringstream oss;
+	tm buf;
+	localtime_s(&buf ,&t);
+	oss << std::put_time(&buf, "%d/%m/%y %H:%M:%S ");
+
 	switch (level) {
 	case eLogLevel::WARNING:
-		OutputDebugString("WARNING::");
+		oss << "WARNING::";
 		break;
 	case eLogLevel::ERROR_:
-		OutputDebugString("ERROR::");
+		oss << "ERROR::";
 		break;
 	case eLogLevel::INFO:
 		break;
 	case eLogLevel::FATAL:
-		OutputDebugString("FATAL::");
+		oss << "FATAL::";
 	}
-	OutputDebugString(msg);
-	OutputDebugString("\n");
+	oss << msg << "\n";
+
+	OutputDebugString(oss.str().c_str());
 
 	if (level == eLogLevel::FATAL) {
 		assert(false && msg);
