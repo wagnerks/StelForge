605a42ef7c3839a781012def44af295a939f1c7e
Kirill Vagner <stalker1177@mail.ru>
2023-10-17 22:25:18 +0200

    fix shaders system accroding new ecs interface

--- src/systemsModule/ShaderSystem.cpp
+++ src/systemsModule/ShaderSystem.cpp
@@ -9,28 +9,32 @@
 
 namespace Engine::SystemsModule {
 	void ShaderSystem::update(float_t dt) {
-		auto shaderController = ShaderModule::ShaderController::instance();
-		drawableEntities.clear();
-		
-		for (auto [shaderComponent] : ECSHandler::registry()->getComponentsArray<ComponentsModule::ShaderComponent>()) {
-			auto shader = shaderController->getShader(shaderComponent.shaderId);
-			if (!shader) {
-				continue;
+		auto compsArr = ECSHandler::registry()->getComponentsArray<ComponentsModule::ShaderComponent>();
+		if (!compsArr.empty()) {
+			auto shaderController = ShaderModule::ShaderController::instance();
+			drawableEntities.clear();
+
+			for (auto [entity, shaderComponent] : ECSHandler::registry()->getComponentsArray<ComponentsModule::ShaderComponent>()) {
+				auto shader = shaderController->getShader(shaderComponent.shaderId);
+				if (!shader) {
+					continue;
+				}
+
+				if (shaderComponent.updateFunction) {
+					shaderComponent.updateFunction(&shaderComponent);
+				}
+
+				for (const auto typedUniforms : shaderComponent.uniforms) {
+					typedUniforms->apply(shader);
+				}
+
+				drawableEntities.emplace_back(shaderComponent.shaderId, entity);
 			}
 
-			if (shaderComponent.updateFunction) {
-				shaderComponent.updateFunction(&shaderComponent);
-			}
-
-			for (const auto typedUniforms : shaderComponent.uniforms) {
-				typedUniforms->apply(shader);
-			}
-
-			drawableEntities.emplace_back(shaderComponent.shaderId, shaderComponent.getEntityId());
+			std::stable_sort(drawableEntities.begin(), drawableEntities.end(), [](const auto& a, const auto& b) {
+				return a.first > b.first;
+			});
 		}
-
-		std::stable_sort(drawableEntities.begin(), drawableEntities.end(), [](const auto& a, const auto& b) {
-			return a.first > b.first;
-		});
+		
 	}
 }
